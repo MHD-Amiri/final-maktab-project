8cb66e9341aea19d0db7eaccc7efc28f63a7c788
MHD-Amiri <mohamadamin.mhd@gmail.com>
2020-07-20 00:43:41 +0430

    add user info to dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authenticate = require('../config/auth');
+
+// User model
+const User = require("../models/user");
+
 // GET home page
 router.get('/', (req, res, next) => {
   res.render('pages/home', {
@@ -14,6 +18,23 @@ router.get('/dashboard', authenticate, (req, res, next) => {
     title: 'Dashboard',
     firstName: req.user.firstName,
   });
-})
+});
+
+// GET User Information for Dashboard
+router.get('/dashboard/:userId', (req, res, next) => {
+  const id = req.params.userId;
+  User.findById(id)
+    .exec()
+    .then(doc => {
+      console.log('From Database', doc);
+      res.status(200).json(doc);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
 
 module.exports = router;
