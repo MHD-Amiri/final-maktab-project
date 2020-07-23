f71b2e75be9a1387931f3ca1d7661bdaeb160940
MHD-Amiri <mohamadamin.mhd@gmail.com>
2020-07-23 06:07:48 +0430

    add hashed password to Admin user

--- tools/initialization.js
+++ tools/initialization.js
@@ -1,14 +1,18 @@
 const User = require("../models/user");
+const bcrypt = require('bcryptjs');
 
+// Initialize of the ADMIN
 const initialization = async function () {
     try {
-        const EXIST_ADMIN = await User.findOne({
+        // Check if there is an ADMIN
+        const ADMIN_EXISTS = await User.findOne({
             role: 'admin'
         });
-        if (EXIST_ADMIN) {
+        if (ADMIN_EXISTS) {
             return console.log('Admin Done!');
         };
 
+        // make an ADMIN
         const ADMIN = new User({
             firstName: 'ali',
             lastName: 'kuchulu',
@@ -17,13 +21,23 @@ const initialization = async function () {
             password: "123456789",
             gender: 'Male',
             phoneNumber: '09125544712',
-            role: 'admin',
-            password: '12345678'
+            role: 'admin'
         });
+        // Hash password
+        bcrypt.genSalt(10, (err, salt) => bcrypt.hash(ADMIN.password, salt, (err, hash) => {
+            if (err) throw err;
+            // Set the password to hashed password
+            ADMIN.password = hash;
+            ADMIN.save()
+                .then(() => {
+                    console.log('Admin created');
+                })
+                .catch(err => {
+                    console.log("ADMIN initialize did not work!");
+                    
+                })
+        }));
 
-        await ADMIN.save();
-
-        console.log('Admin created');
     } catch (err) {
         console.log('Error in intialization function', err);
     };
