8447cacd1492f9ced5326a4982a2936792b36f74
MHD-Amiri <mohamadamin.mhd@gmail.com>
2020-08-04 01:23:39 +0430

    Add edit.js and config it for getting edit page

---
+++ routes/edit.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const authenticate = require('../config/auth');
+
+// User model
+const User = require("../models/user");
+
+
+// GET Edit page
+router.get('/', authenticate, (req, res, next) => {
+    if (req.cookies.user_sid && !req.session.passport.user) {
+      res.clearCookie("user_sid");
+    };
+    res.render('pages/edit', {
+      title: 'Edit Profile',
+      firstName: req.user.firstName,
+      lastName: req.user.lastName,
+      userName: req.user.userName,
+      gender: req.user.gender,
+      phoneNumber: req.user.phoneNumber,
+      bio: req.user.bio
+    });
+  });
+
+module.exports = router;
