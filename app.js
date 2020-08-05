38c21e9bc4a554713d97d63e6da4698a2b58389c
MHD-Amiri <mohamadamin.mhd@gmail.com>
2020-08-05 20:40:48 +0430

    Add article route

--- app.js
+++ app.js
@@ -66,8 +66,10 @@ app.use((req, res, next) => {
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const editRouter = require('./routes/edit');
+const articleRouter = require('./routes/article');
 const passport = require('./config/passport');
 app.use('/dashboard/edit', editRouter);
+app.use('/dashboard/article', articleRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
