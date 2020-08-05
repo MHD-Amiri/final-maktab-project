4f7681e1b39ef5422506031e339b6683bec807ad
MHD-Amiri <mohamadamin.mhd@gmail.com>
2020-08-05 20:42:26 +0430

    Create and add article page sender

---
+++ routes/article.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const authenticate = require('../config/auth');
+
+// User model
+const User = require("../models/user");
+
+// GET Article page
+router.get('/', authenticate, (req, res, next) => {
+  if (req.cookies.user_sid && !req.session.passport.user) {
+    res.clearCookie("user_sid");
+  };
+  res.render('pages/article', {
+    title: 'New Article',
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
+    userName: req.user.userName,
+    gender: req.user.gender,
+    phoneNumber: req.user.phoneNumber,
+    bio: req.user.bio
+  });
+});
+
+module.exports = router;
